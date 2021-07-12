9bafaa54f111dbef7936709291873c608f52a302
OMEN44 <huonswales@gmail.com>
2021-07-12 17:36:06 +1000

    progress was made a bit

--- SpigotPlugins/src/main/java/indrocraft/spigot/ecnomyranks/Main.java
+++ SpigotPlugins/src/main/java/indrocraft/spigot/ecnomyranks/Main.java
@@ -1,5 +1,6 @@
 package indrocraft.spigot.ecnomyranks;
 
+import indrocraft.spigot.ecnomyranks.commands.Complaints;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
@@ -9,7 +10,7 @@ public final class Main extends JavaPlugin {
         // Plugin startup logic
         getLogger().info("SpigotPlugin.Ranks-Economy Enabled");
 
-
+        getServer().getPluginCommand("Complaints").setExecutor(new Complaints(this));
 
     }
 
---
+++ SpigotPlugins/src/main/java/indrocraft/spigot/ecnomyranks/commands/Complaints.java
@@ -0,0 +1,40 @@
+package indrocraft.spigot.ecnomyranks.commands;
+
+import indrocraft.spigot.ecnomyranks.Main;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Complaints implements CommandExecutor {
+    //this is so that i can message console:
+    private final Main main;
+
+    public Complaints(Main main) {
+        this.main = main;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        //checks if the console issued the command
+        if (!(sender instanceof Player)) {
+            main.getLogger().info("Must be a player to use this command!");
+            return true;
+        }
+        //casts player
+        Player player = (Player) sender;
+        //puts the the args into a string
+        String msg = "";
+        for (int i = 0; i < args.length; i++) {
+            msg += " " + args[i];
+        }
+        player.sendMessage("final message is: " + ChatColor.BLUE + "" + msg);
+        //sends message to admin
+        Player admin1 = Bukkit.getPlayer("OMEN44");
+        admin1.sendMessage("Sup, some one said dis: " + msg);
+
+        return true;
+    }
+}
