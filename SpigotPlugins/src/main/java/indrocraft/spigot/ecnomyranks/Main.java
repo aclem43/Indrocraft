7c3908c96db1c09a2c9d79122772ab6c893547ff
Aclem43 <aclemeur@gmail.com>
2021-07-12 16:47:55 +1000

    Quick Setup

--- SpigotPlugins/src/main/java/indrocraft/spigot/ecnomyranks/Main.java
+++ SpigotPlugins/src/main/java/indrocraft/spigot/ecnomyranks/Main.java
@@ -7,6 +7,8 @@ public final class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        getLogger().info("SpigotPlugin.Ranks-Economy Enabled");
+
 
 
     }
