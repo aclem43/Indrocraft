dc59b04d8e0e0572480de3077b67dd54588748d7
aclemeur@gmail.com <aclemeur@gmail.com>
2021-07-12 16:36:50 +1000

    Initial commit

---
+++ SpigotPlugins/src/main/java/indrocraft/spigot/ecnomyranks/Main.java
@@ -0,0 +1,18 @@
+package indrocraft.spigot.ecnomyranks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
