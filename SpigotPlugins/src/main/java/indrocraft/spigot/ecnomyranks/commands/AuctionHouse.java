6f8faedf823d7a44139bf764d30fd2fd2794d936
Aclem43 <aclemeur@gmail.com>
2021-07-31 14:20:40 +1000

    Auction House Init Page

--- SpigotPlugins/src/main/java/indrocraft/spigot/ecnomyranks/commands/AuctionHouse.java
+++ SpigotPlugins/src/main/java/indrocraft/spigot/ecnomyranks/commands/AuctionHouse.java
@@ -1,5 +1,21 @@
 package indrocraft.spigot.ecnomyranks.commands;
 
-public class AuctionHouse {
+import indrocraft.spigot.ecnomyranks.inventories.InitAH;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
+public class AuctionHouse implements CommandExecutor {
+
+
+    public boolean onCommand( CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player){
+            Player player = (Player) sender;
+            InitAH gui = new InitAH();
+            player.openInventory(gui.getInventory());
+        }
+        return true;
+    }
 }
