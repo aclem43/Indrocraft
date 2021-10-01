d8b581f3e1a5d90ee6faf82255fece8e3d041b60
OMEN44 <hswal2@eq.edu.au>
2021-10-01 10:18:16 +1000

    made progress on dynamic ranks

--- SpigotPlugins/src/main/java/indrocraft/spigot/ecnomyranks/events/RankEvents.java
+++ SpigotPlugins/src/main/java/indrocraft/spigot/ecnomyranks/events/RankEvents.java
@@ -1,9 +1,11 @@
 package indrocraft.spigot.ecnomyranks.events;
 
 import indrocraft.spigot.ecnomyranks.Main;
+import indrocraft.spigot.ecnomyranks.databasemanager.Databasemanager;
 import indrocraft.spigot.ecnomyranks.databasemanager.SQLgetter;
 import indrocraft.spigot.ecnomyranks.ranks.RankUtils;
 import org.bukkit.NamespacedKey;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,16 +17,16 @@ public class RankEvents implements Listener {
 
     @EventHandler
     public void advancementDoneEvent(PlayerAdvancementDoneEvent event) {
-        Player player = event.getPlayer();
-        NamespacedKey key = event.getAdvancement().getKey();
-        int level = RankUtils.getLevel(player, main.data);
-        int getGod = main.data.getInt(player.getUniqueId(), "god", "playerinfo");
-        if (key.getNamespace().equals(NamespacedKey.MINECRAFT)) {
-            if (key.getKey().equals("story/mine_diamond")) {
-                if (level == 0) {
-
-                }
+        FileConfiguration config = Databasemanager.getFileConfig("config.yml");
+        if (config.getBoolean("autoRankUp")) {
+            Player player = event.getPlayer();
+            NamespacedKey key = event.getAdvancement().getKey();
+            int level = RankUtils.getLevel(player, main.data);
+            String advancement = config.getString("0.achievement");
+            if (advancement.equalsIgnoreCase(key.getKey())) {
+                RankUtils.setRank(player, main.data, config.getString("0.nextRank"));
             }
+            int getGod = main.data.getInt(player.getUniqueId(), "god", "playerinfo");
         }
     }
 }
